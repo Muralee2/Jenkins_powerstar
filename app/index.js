492ab18966757c531269aebec4437a2c6fe9f75b
unknown <muralimvkaws1@gmail.com>
2025-07-31 11:49:33 +0530

    Add app/index.js for Jenkins DevOps practice

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Jenkins DevOps Pipeline!');
+});
+
+server.listen(3000, () => {
+  console.log('🚀 Server running on port 3000');
+});
+
